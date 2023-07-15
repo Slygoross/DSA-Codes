d71565197378d569783c93a2f0f08506b126765b
Pranav Modgil <pranavmodgil@gmail.com>
2023-07-15 21:47:15 +0530

    229 without optimal

--- 229. Majority Element II.cpp
+++ 229. Majority Element II.cpp
@@ -1,3 +1,30 @@
+//Brute Force:
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        
+        for(int i = 0; i < n; i++){
+            if (ans.size() == 0 || ans[0] != nums[i]){
+                int cnt = 0;
+                for(int j = 0; j < n; j++){
+                    if(nums[i]==nums[j]){
+                        cnt++;
+                    }
+                }
+                if(cnt>(n/3)){
+                    ans.push_back(nums[i]);
+                }
+            }
+            if(ans.size()==2){
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
 // Better 
 class Solution {
 public:
