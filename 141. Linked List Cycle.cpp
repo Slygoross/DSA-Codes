0cba6120dd2bacd6d7b25c339b70a6711f5bff61
Pranav Modgil <pranavmodgil@gmail.com>
2023-08-21 09:28:29 +0530

    141

---
+++ 141. Linked List Cycle.cpp
@@ -0,0 +1,51 @@
+// HashTable Approach: O(N)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL){
+            return false;
+        }
+        unordered_set<ListNode*> hashTable;
+        while(head != NULL){
+            if(hashTable.find(head) != hashTable.end()){
+                return true;
+            }
+            hashTable.insert(head);
+            head = head -> next;
+        }
+        return false;
+    }
+};
+
+// Two Pointers Approach: O(N)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast -> next != NULL && fast -> next -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+};
