3e26073bf3044d511d05eefb8d70c90db5db868d
Pranav Modgil <pranavmodgil@gmail.com>
2023-10-13 09:07:20 +0530

    61 Optimal

--- 61. Rotate List.cpp
+++ 61. Rotate List.cpp
@@ -1,4 +1,4 @@
-//Brute Force:
+//Brute Force: (TLE)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -21,6 +21,44 @@ public:
             end->next = head;
             head = end;
         }
+        return head;
+    }
+};
+
+//Optimal
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL || head->next == NULL || k == 0){
+            return head;
+        }
+
+        ListNode* temp = head;
+        int length = 1;
+        while(temp->next != NULL){
+            ++length;
+            temp = temp -> next;
+        }
+
+        temp->next = head;
+        k = k % length;
+        int end = length - k;
+        while(end--){
+            temp = temp -> next;
+        }
+        head = temp -> next;
+        temp -> next = NULL;
+
         return head;
     }
 };
