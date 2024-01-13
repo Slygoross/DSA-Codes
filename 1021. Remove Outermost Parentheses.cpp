b3572411c63133c5c19a2a0f8c4a50783a5c9f1d
Pranav Modgil <pranavmodgil@gmail.com>
2024-01-13 09:40:28 +0530

    1021 Better + Optimal

--- 1021. Remove Outermost Parentheses.cpp
+++ 1021. Remove Outermost Parentheses.cpp
@@ -16,3 +16,22 @@ public:
     return result;
     }
 };
+
+// Better & Optimal
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";
+        int count = 0;
+
+        for(char c : s){
+            if( c == '(' && count++ > 0){
+                result += '(';
+            }
+            else if( c == ')' && --count > 0){
+                result += ')';
+            }
+        }
+        return result;
+    }
+};
