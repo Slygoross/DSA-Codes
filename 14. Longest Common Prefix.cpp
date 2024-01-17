62bcf7f7756f58e182aae6f76a7cac627bd558db
Pranav Modgil <pranavmodgil@gmail.com>
2024-01-17 23:04:50 +0530

    Refresh

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,27 @@
+//Bruteforce:
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) {
+        return "";
+    }
+
+    // Iterate through characters of the first string
+    for (int i = 0; i < strs[0].size(); i++) {
+        char currentChar = strs[0][i];
+
+        // Check if the current character is common to all strings
+        for (int j = 1; j < strs.size(); j++) {
+            if (i >= strs[j].size() || strs[j][i] != currentChar) {
+                // If not common, return the prefix found so far
+                return strs[0].substr(0, i);
+            }
+        }
+    }
+
+    // If all characters match, return the entire first string as the common prefix
+    return strs[0];
+    }
+};
+
+//
