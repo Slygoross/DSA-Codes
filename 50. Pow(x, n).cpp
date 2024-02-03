dc34c43645158e7fc220248c20193bec63693bb1
Pranav Modgil <pranavmodgil@gmail.com>
2024-02-03 20:22:06 +0530

    50

---
+++ 50. Pow(x, n).cpp
@@ -0,0 +1,44 @@
+//Bruteforce(TLE)
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        double ans = 1;
+        
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                ans *= x;
+            }
+        } else {
+            for (int i = 0; i > n; i--) {
+                ans /= x;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//Optimal (Binary Exponentiation)
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+  long long nn = n;
+  if (nn < 0) nn = -1 * nn;
+  while (nn) {
+    if (nn % 2) {
+      ans = ans * x;
+      nn = nn - 1;
+    } else {
+      x = x * x;
+      nn = nn / 2;
+    }
+  }
+  if (n < 0) ans = (double)(1.0) / (double)(ans);
+  return ans;
+    }
+};
